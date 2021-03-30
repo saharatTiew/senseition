009ec46a8ce04ba0ca2f0a1b2137fb050166d901
Phanuwat S <ps13james@gmail.com>
2021-03-31 00:12:36 +0700

    Completed design ui review

--- src/components/pages/entries/Background.jsx
+++ src/components/pages/entries/Background.jsx
@@ -1,7 +1,7 @@
 export default function Background() {
     return (
         <img
-            src="https://thumbs.dreamstime.com/b/europe-building-clock-tower-abac-university-bangna-bangko-bangkok-thailand-76318963.jpg"
+            src="https://i.pinimg.com/originals/19/a7/e4/19a7e4760cf946d3230fb1e96457af78.jpg"
             className="bg"
             alt="university"
         />
---
+++ src/components/pages/reviews/Review.jsx
@@ -0,0 +1,63 @@
+import {Box, Paper, Typography} from "@material-ui/core";
+import {Row} from "react-bootstrap";
+import React from "react";
+import Divider from "@material-ui/core/Divider";
+
+const data = [
+    {
+        id: 1,
+        firstName: "Phanuwat",
+        lastName: "Sikharestrakul",
+        profileURL: "",
+        posting: "jferuikfhjeruiofjeruifh",
+        rate: "3"
+    },
+    {
+        id: 2,
+        firstName: "Phanuwat",
+        lastName: "Sikharestrakul",
+        profileURL: "",
+        posting: "jferuikfhjeruiofjeruifh",
+        rate: "3"
+    }
+]
+
+function Review(props) {
+    return (
+        <Paper className="wrapper" style={{marginTop: 30, marginRight: 100, marginLeft: 100, marginBottom: 30, padding: 50, maxHeight: 500}}>
+            <Row>
+                <div>
+                    <img
+                        src="https://cdn1.iconfinder.com/data/icons/freeline/32/account_friend_human_man_member_person_profile_user_users-512.png"
+                        width="32px" height="32px"
+                        alt="human"
+                        className="mr-5"
+                    />
+                </div>
+                <div>
+                    <h4 className="mr-large">Phanuwat Sikharestrakul</h4>
+                </div>
+                <div>
+                    <h4>Rate: 3/5</h4>
+                </div>
+            </Row>
+            <div>
+                <Typography component="div">
+                    <Box textAlign="justify" m={4}>
+                        Ambitioni dedisse scripsisse iudicaretur. Cras mattis iudicium purus sit amet fermentum.
+                        Donec sed odio operae, eu vulputate felis rhoncus. Ambitioni dedisse scripsisse iudicaretur.
+                        Cras mattis iudicium purus sit amet fermentum.
+                        Donec sed odio operae, eu vulputate felis rhoncus. Ambitioni dedisse scripsisse iudicaretur.
+                        Cras mattis iudicium purus sit amet fermentum.
+                        Donec sed odio operae, eu vulputate felis rhoncus. Ambitioni dedisse scripsisse iudicaretur.
+                        Cras mattis iudicium purus sit amet fermentum.
+                        Donec sed odio operae, eu vulputate felis rhoncus.
+                    </Box>
+                </Typography>
+                <Divider />
+            </div>
+        </Paper>
+    )
+}
+
+export default Review
