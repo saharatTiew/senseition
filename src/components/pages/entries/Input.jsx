b9f129c82cdbb2c0a84b0883e7349ce9327c631a
Phanuwat S <ps13james@gmail.com>
2021-03-31 10:34:35 +0700

    Completed connect API Register

--- src/components/pages/entries/Input.jsx
+++ src/components/pages/entries/Input.jsx
@@ -14,14 +14,12 @@ function LoginInput(props) {
 }
 
 function RegisterInput(props) {
-  const { register, handleSubmit, control, watch, errors } = useForm();
-  console.log(errors)
   return (
     <input
       type={props.type}
       placeholder={props.placeholder}
       className="border border-dark text-secondary login-btn register-input test2"
-      ref={register({
+      ref={props.register({
         required: true
       })}
       name={props.name}
